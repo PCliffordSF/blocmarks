6cd0dbfd6503d737656b08d5c7670bf8b9262640
Paul Clifford <paulclifford2570@gmail.com>
2017-06-27 13:41:55 +0000

    finish 5_chkp

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -52,14 +52,14 @@ class TopicsController < ApplicationController
     puts '==========================='
     puts params
     puts '========================================'
-    @bookmark = Bookmark.find(params[:id])
+    @bookmark = Bookmark.find(params[:bookmark_id])
      
     if @bookmark.destroy
       flash[:notice] = "\"#{@bookmark.url}\" was deleted successfully."
-      redirect_to topic_path(params[:format])
+      redirect_to topic_path(params[:id])
     else
       flash.now[:alert] = "There was an error deleting the topic."
-      redirect_to topic_path(params[:format])
+      redirect_to topic_path(params[:id])
     end
     
   end
