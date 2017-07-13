5d2a83cb7813a9544d11feaf39a21d9708c9bc7e
Paul Clifford <paulclifford2570@gmail.com>
2017-07-13 00:02:21 +0000

    add img preview

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -10,19 +10,18 @@ class Bookmark < ActiveRecord::Base
     embedly_api =
     Embedly::API.new :key => '08e70a52f3dd4ac7b046be259dcc0e57',
                     :user_agent => 'Mozilla/5.0 (compatible; Blocmarks/1.0; paulclifford2570@gmail.com)'
+                    #ENV['EMBEDLY_API_KEY']
 
     obj = embedly_api.oembed :url => input_url
-    puts 'HTMLHTMLHTML'
     video = obj.first.html
-    puts video
-    puts 'ThumbnailThumnail'
     thumbnail = obj.first.thumbnail_url
-    puts thumbnail
     #json_obj = JSON.pretty_generate(obj[0].marshal_dump)
-    if video != nil
-        video
-    else
-        thumbnail
-    end
+    # if !video.nil?
+    #     video
+    # else
+    #     thumbnail
+    # end
+    return video unless video.nil?
+    thumbnail
   end
 end
